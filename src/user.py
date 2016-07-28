f4f4c2a6bcf169fff7ce39fac88f72d837a9e29f
AngelGus79 <agustavo79@gmail.com>
2016-07-28 14:04:15 -0500

    actualizacion de getGender user.py

--- src/user.py
+++ src/user.py
@@ -1,6 +1,6 @@
 #sexo 0 indefinido, 1 hombre y 2 mujer
 #Sentimiento 0 pesimo 1 malo 2 regular 3 bueno 4 excelente
-
+import requests
 class User:
     def __init__(self):
         self.name = ""
@@ -10,7 +10,13 @@ class User:
         return 0
     
     def getGender(self):
-        return 0
+        name = self.name
+        payload = {'name': name}
+        response = requests.get('https://api.genderize.io', params=payload)
+        d = response.json()
+        genero = ""
+        genero = d['gender']
+        return  genero
         
 
 

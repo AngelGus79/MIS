37985668725420b2a5939e351b58500ee957526f
César Antáres <zzantares@gmail.com>
2016-07-28 13:58:13 -0500

    Crea los tests para el tweet navigator con mock de twython

--- test/tweetNavigator_test.py
+++ test/tweetNavigator_test.py
@@ -8,7 +8,8 @@ test_path = os.path.abspath(os.path.join(__file__, os.pardir))
 project_path = os.path.abspath(os.path.join(test_path, os.pardir))
 sys.path.append(project_path)
 
-from src.tweetNavigator import tweetNavigator
+from src.tweetNavigator import tweetNavigator, User
+
 
 class TweetNavigatorTest(unittest.TestCase):
 
@@ -16,13 +17,27 @@ class TweetNavigatorTest(unittest.TestCase):
     def test_getUsers(self, mock):
         mock().search.return_value = {
             'statuses': [
-                {'user': {'name': 'Juan'}},
-                {'user': {'name': 'Pedro'}},
+                {
+                    'user': {'name': 'Juan'},
+                    'text': 'Hola',
+                    'id_str': '1122'
+                },
+                {
+                    'user': {'name': 'Pedro'},
+                    'text': 'Adios',
+                    'id_str': '1234'
+                },
             ]
         }
 
         navigator = tweetNavigator('sayulita')
-        self.assertEqual(['Juan', 'Pedro'], navigator.getUsers())
+        users = navigator.getUsers()
+
+        self.assertEqual(2, len(users))
+        user = users['1122']
+        self.assertIsInstance(user, User)
+        self.assertEqual('Juan', user.name)
+        self.assertEqual(['Hola'], user.comments)
 
 
 if __name__ == '__main__':

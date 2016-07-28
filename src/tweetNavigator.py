bea025a319d1e821e77f919fc70d5baf4435a1f1
César Antáres <zzantares@gmail.com>
2016-07-28 15:06:52 -0500

    Agrega análisis de sentimiento a los users.comments
    Ahora los comentarios son diccionarios con las llaves "text" (el
    comentario del usuario) y "sentiment" que tiene el sentimiento tal cual
    dado por mashape.

--- src/tweetNavigator.py
+++ src/tweetNavigator.py
@@ -2,6 +2,7 @@
 from twython import Twython
 from user import User
 import settings
+import requests
 
 
 class tweetNavigator:
@@ -14,8 +15,28 @@ class tweetNavigator:
             oauth_token=settings.TWITTER_ACCESS_TOKEN,
             oauth_token_secret=settings.TWITTER_ACCESS_TOKEN_SECRET)
 
+    def getSentiment(self, text):
+        endpoint = 'https://japerk-text-processing.p.mashape.com/sentiment/'
+        headers = {
+            'X-Mashape-Key': settings.MASHAPE_KEY,
+        }
+
+        payload = {
+            'language': 'english',
+            'text': text,
+        }
+
+        response = requests.post(endpoint, headers=headers, data=payload)
+        # {"probability": {"neg": 0.36525227438916918, "neutral": 0.56068716327814216, "pos": 0.63474772561083082}, "label": "neutral"}
+        response = response.json()
+
+        return {
+            'text': text,
+            'sentiment': response['label'],
+        }
+
     def getUsers(self):
-        search = self.twython.search(q=self.hashtag)
+        search = self.twython.search(q=self.hashtag, count=3)
 
         tweets = search['statuses']
         users_dictionary = {}
@@ -25,11 +46,17 @@ class tweetNavigator:
 
             if not users_dictionary.has_key(tweet['id_str']):
                 u.name = tweet["user"]["name"].encode("utf-8")
-                u.comments.append(tweet['text'])
+
+                comment = self.getSentiment(tweet['text'])
+                u.comments.append(comment)
+
                 users_dictionary[tweet['id_str']] = u
             else:
                 u = users_dictionary[tweet['id_str']]
-                u.comments.append(tweet['text'])
+
+                comment = self.getSentiment(tweet['text'])
+                u.comments.append(comment)
+
                 users_dictionary[tweet['id_str']] = u
 
         return users_dictionary

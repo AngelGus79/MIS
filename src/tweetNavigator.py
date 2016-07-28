c98da52beb281bca2e7474972d0499ad03648f92
César Antáres <zzantares@gmail.com>
2016-07-28 13:55:29 -0500

    Refactoriza tweetNavigator para evitar multiples instancias de twython
    Adicional evita hacer llamada extra que hace que los tests no pasen.

--- src/tweetNavigator.py
+++ src/tweetNavigator.py
@@ -1,42 +1,38 @@
+# -*- coding: utf-8 -*-
 from twython import Twython
 from user import User
 import settings
+
 
 class tweetNavigator:
     def __init__(self, hashtag):
         self.hashtag = hashtag
 
-    def getUsers(self):
-        t = Twython(app_key=settings.TWITTER_APP_KEY,
-        app_secret=settings.TWITTER_APP_KEY_SECRET,
-        oauth_token=settings.TWITTER_ACCESS_TOKEN,
-        oauth_token_secret=settings.TWITTER_ACCESS_TOKEN_SECRET)
+        self.twython = Twython(
+            app_key=settings.TWITTER_APP_KEY,
+            app_secret=settings.TWITTER_APP_KEY_SECRET,
+            oauth_token=settings.TWITTER_ACCESS_TOKEN,
+            oauth_token_secret=settings.TWITTER_ACCESS_TOKEN_SECRET)
 
-        search = t.search(q=self.hashtag)
+    def getUsers(self):
+        search = self.twython.search(q=self.hashtag)
 
         tweets = search['statuses']
         users_dictionary = {}
-        u = User()
+
         for tweet in tweets:
+            u = User()
+
             if not users_dictionary.has_key(tweet['id_str']):
                 u.name = tweet["user"]["name"].encode("utf-8")
                 u.comments.append(tweet['text'])
                 users_dictionary[tweet['id_str']] = u
             else:
-                u=users_dictionary[tweet['id_str']]
+                u = users_dictionary[tweet['id_str']]
                 u.comments.append(tweet['text'])
                 users_dictionary[tweet['id_str']] = u
-              
+
         return users_dictionary
-    
+
     def getComments(self):
         print "algo"
-
-
-tn = tweetNavigator("#sayulita")
-usuarios =tn.getUsers()
-
-for u in usuarios:
-    print "Usuario:" + usuarios[u].name + "\n"
-    for c in usuarios[u].comments:
-        print "Commentarios: " + c.encode("utf-8") + "\n"

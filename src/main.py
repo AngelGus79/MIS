6d997aef8703c4e4789d6a3ef12273ac8c5fbabe
César Antáres <zzantares@gmail.com>
2016-07-28 10:50:09 -0500

    Ejemplo: Determinar el genero de los usuarios a partir de una lista de nombres

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 from twython import Twython
 import settings
 
+# Ejemplo de tweets
 t = Twython(app_key=settings.TWITTER_APP_KEY,
             app_secret=settings.TWITTER_APP_KEY_SECRET,
             oauth_token=settings.TWITTER_ACCESS_TOKEN,
@@ -13,4 +14,11 @@ tweets = search['statuses']
 
 for tweet in tweets:
     print tweet
-   
+
+# Ejemplo de genero
+import requests
+names = ['John', 'Lauren', 'Peter']
+
+payload = {'name[]': names}
+response = requests.get('https://api.genderize.io', params=payload)
+print response.json()

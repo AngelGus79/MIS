65fe0256548975a0fbd165d8fa53840149dd8f91
AngelGus79 <agustavo79@gmail.com>
2016-07-28 16:44:46 -0500

    definicion de genero terminado, por grupo e individual

--- src/tweetNavigator.py
+++ src/tweetNavigator.py
@@ -2,6 +2,7 @@
 from twython import Twython
 from user import User
 import settings
+import requests
 
 
 class tweetNavigator:
@@ -19,20 +20,47 @@ class tweetNavigator:
 
         tweets = search['statuses']
         users_dictionary = {}
+        users_list = []
+        
 
         for tweet in tweets:
             u = User()
 
-            if not users_dictionary.has_key(tweet['id_str']):
+            if not users_dictionary.has_key(tweet['user']['id']):
                 u.name = tweet["user"]["name"].encode("utf-8")
-                u.comments.append(tweet['text'])
-                users_dictionary[tweet['id_str']] = u
+                u.comments.append(tweet['text'].encode("utf-8"))
+                users_dictionary[tweet['user']['id']] = u
+                users_list.append(u.getFormatedName())
             else:
-                u = users_dictionary[tweet['id_str']]
-                u.comments.append(tweet['text'])
-                users_dictionary[tweet['id_str']] = u
+                u = users_dictionary[tweet['user']['id']]
+                u.comments.append(tweet['text'].encode("utf-8"))
+                users_dictionary[tweet['user']['id']] = u
 
+        #Asignacion de genero
+        payload = {'name[]': users_list}
+        response = requests.get('https://api.genderize.io', params=payload)
+        genders_dictionary = response.json()
+        
+        for user in users_dictionary:
+            for gender in genders_dictionary:
+                if users_dictionary[user].formated_name == gender["name"].encode('utf-8'):
+                    if not gender["gender"]:
+                        users_dictionary[user].gender = 'undefined'
+                    else:
+                        users_dictionary[user].gender = gender["gender"]
+
+                    genders_dictionary.remove(gender)
+                    break
+      
         return users_dictionary
 
     def getComments(self):
         print "algo"
+
+tn = tweetNavigator("#sayulita")
+usuarios = tn.getUsers()
+
+for u in usuarios:
+    print "Usuario: " + usuarios[u].formated_name + " Genero: " + usuarios[u].gender
+    for c in usuarios[u].comments:
+        print "comentario: " + c
--- src/user.py
+++ src/user.py
@@ -3,9 +3,18 @@
 #Sentimiento 0 pesimo 1 malo 2 regular 3 bueno 4 excelente
 import requests
 class User:
-    def __init__(self):
-        self.name = ""
+    def __init__(self, name = ""):
+        self.name = name
         self.comments = []
+        self.gender = "undefined"
+        self.formated_name = ""
+
+    def getFormatedName(self):
+        formated_name = self.name.strip()
+        list_formated_name = formated_name.split()
+        self.formated_name = list_formated_name[0]
+        return self.formated_name        
+            
         
     def getSentiment(self):
         return 0
@@ -14,10 +23,17 @@ class User:
         name = self.name
         payload = {'name': name}
         response = requests.get('https://api.genderize.io', params=payload)
-        d = response.json()
-        genero = ""
-        genero = d['gender']
-        return  genero
+        gender_dictionary = response.json()
+
+        if not gender_dictionary["gender"]:
+            self.gender = 'undefined'
+        else:
+            self.gender = gender_dictionary["gender"]
+
+        return self.gender
+        
+            
+            
         
 
 

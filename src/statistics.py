62d790f0c75d190848587a94429edcfac1f6f30d
César Antáres <zzantares@gmail.com>
2016-07-28 23:16:31 -0500

    Agrega estadísticas para tener los sentimientos por género
    Ahora es posible ver tener el sentimiento por genero, es decir mujeres,
    hombres y los indefinidos.

--- src/statistics.py
+++ src/statistics.py
@@ -26,3 +26,79 @@ class Statistics:
             'undefined': undefined,
             'total': women + men + undefined,
         }
+
+    def getUsersSentiment(self):
+        positive = 0
+        negative = 0
+        neutral = 0
+
+        # Para guardar conteos de mujeres
+        female_positive = 0
+        female_negative = 0
+        female_neutral = 0
+
+        # Para guardar conteos de hombres
+        male_positive = 0
+        male_negative = 0
+        male_neutral = 0
+
+        # Para guardar conteos de indefinidos
+        undefined_positive = 0
+        undefined_negative = 0
+        undefined_neutral = 0
+
+        for user in self.users:
+            sentiment = user.getSentiment()
+
+            if sentiment == 'positive':
+                positive += 1
+
+                if user.gender == 'female':
+                    female_positive += 1
+                elif user.gender == 'male':
+                    male_positive += 1
+                else:
+                    undefined_positive += 1
+
+            elif sentiment == 'negative':
+                negative += 1
+
+                if user.gender == 'female':
+                    female_negative += 1
+                elif user.gender == 'male':
+                    male_negative += 1
+                else:
+                    undefined_negative += 1
+
+            elif sentiment == 'neutral':
+                neutral += 1
+
+                if user.gender == 'female':
+                    female_neutral += 1
+                elif user.gender == 'male':
+                    male_neutral += 1
+                else:
+                    undefined_neutral += 1
+
+        return {
+            'all': {
+                'positive': positive,
+                'negative': negative,
+                'neutral': neutral,
+            },
+            'female': {
+                'positive': female_positive,
+                'negative': female_negative,
+                'neutral': female_neutral,
+            },
+            'male': {
+                'positive': male_positive,
+                'negative': male_negative,
+                'neutral': male_neutral,
+            },
+            'undefined': {
+                'positive': undefined_positive,
+                'negative': undefined_negative,
+                'neutral': undefined_neutral,
+            },
+        }
--- test/statistics_test.py
+++ test/statistics_test.py
@@ -1,8 +1,6 @@
-import mock
 import unittest
 import os
 import sys
-import httpretty
 
 # Update python path
 test_path = os.path.abspath(os.path.join(__file__, os.pardir))
@@ -15,28 +13,62 @@ from src.statistics import Statistics
 
 class TweetNavigatorTest(unittest.TestCase):
 
-    def test_getUsersCount(self):
+    def setUp(self):
         juan = User(name='Juan')
         juan.gender = 'male'
+        juan.comments = [
+            {'text': 'Hola mundo', 'sentiment': 'pos'},
+        ]
 
         fatima = User(name='Fatima')
         fatima.gender = 'female'
+        fatima.comments = [
+            {'text': 'Hola mundo', 'sentiment': 'pos'},
+        ]
 
         pedro = User(name='Pedro')
         pedro.gender = 'male'
+        pedro.comments = [
+            {'text': 'Hola mundo', 'sentiment': 'neg'},
+        ]
 
-        users = {
+        self.users = {
             '1234': juan,
             '1010': fatima,
             '2222': pedro,
         }
 
-        s = Statistics(users)
+    def test_getUsersCount(self):
+        s = Statistics(self.users)
         counts = s.getUsersCount()
         self.assertEqual(2, counts['male'])
         self.assertEqual(1, counts['female'])
         self.assertEqual(0, counts['undefined'])
         self.assertEqual(3, counts['total'])
+
+    def test_getUsersSentiment(self):
+        s = Statistics(self.users)
+        sentiments = s.getUsersSentiment()
+
+        # Conteo de sentimientos por todos los usuarios
+        self.assertEqual(2, sentiments['all']['positive'])
+        self.assertEqual(1, sentiments['all']['negative'])
+        self.assertEqual(0, sentiments['all']['neutral'])
+
+        # Conteo de sentimientos solo female
+        self.assertEqual(1, sentiments['female']['positive'])
+        self.assertEqual(0, sentiments['female']['negative'])
+        self.assertEqual(0, sentiments['female']['neutral'])
+
+        # Conteo de sentimientos solo male
+        self.assertEqual(1, sentiments['male']['positive'])
+        self.assertEqual(1, sentiments['male']['negative'])
+        self.assertEqual(0, sentiments['male']['neutral'])
+
+        # Conteo de sentimientos solo indefinidos
+        self.assertEqual(0, sentiments['undefined']['positive'])
+        self.assertEqual(0, sentiments['undefined']['negative'])
+        self.assertEqual(0, sentiments['undefined']['neutral'])
 
 if __name__ == '__main__':
     unittest.main()

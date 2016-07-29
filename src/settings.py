d3bc07116d292c2179253c91ff2215db52c4a47c
César Antáres <zzantares@gmail.com>
2016-07-29 14:58:42 -0500

    Agrega archivo de settings

---
+++ src/settings.py
@@ -0,0 +1,8 @@
+# Llaves de acceso a twitter
+TWITTER_APP_KEY = ''
+TWITTER_APP_KEY_SECRET = ''
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = ''
+
+# Llaves de acceso a Mashape
+MASHAPE_KEY = ''
